cb3fef68e64a539e2f3bc5abf7790d254739c4f7
Tech With Taha <67154950+taha-lyousfi@users.noreply.github.com>
2021-01-22 14:02:29 +0100

    something

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import flask
 from flask import request, jsonify
 import sqlite3
+import os
+port = int(os.environ.get('PORT', 5000))
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
@@ -68,4 +70,4 @@ def api_filter():
     return jsonify(results)
 
 
-app.run()
+app.run(host='0.0.0.0', port=port, debug=True)
