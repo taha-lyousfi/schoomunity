14589114ab0f01e53e76ecff2d8e7ae3fa2f636e
Tech With Taha <67154950+taha-lyousfi@users.noreply.github.com>
2021-02-01 15:07:11 +0100

    Change database_url

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # Purpose: Configuration constants for the flask app
 
-SQLALCHEMY_DATABASE_URI = "postgres://xubxdpwq:lcKcbnPkMANWQ4hObi26U9DXfzWjkEM1@ziggy.db.elephantsql.com:5432/xubxdpwq"
+SQLALCHEMY_DATABASE_URI = "postgres://zdwovurjbcifuk:f72be6dc714c077bf2b291a8d1267ea7ec99253381098f6fb08198cd84a0a4e7@ec2-52-6-45-247.compute-1.amazonaws.com:5432/d1cheosfq6ghjk"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
